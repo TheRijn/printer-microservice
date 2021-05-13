575006ae9ec5afc21f222a12d0d58cda4849d745
Marijn Doeve <marijn@doeve.me>
2021-05-13 15:49:58 +0200

    Update and no more jQuery

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from werkzeug.datastructures import FileStorage
 app = Flask(__name__)
 
 ALLOWED_EXTENSIONS = {'pdf'}
-DO_NOT_PRINT = False
+DO_NOT_PRINT = True
 
 
 def allowed_file(filename):
@@ -26,34 +26,35 @@ def print_job():
     if 'file' not in request.files:
         return "false"
     file: FileStorage = request.files['file']
+
     # if user does not select file, browser also
     # submit an empty part without filename
-
     if file.filename == '':
         return "false"
-    if file and allowed_file(file.filename):
-        # Create print process
+    if not file and not allowed_file(file.filename):
+        return "false"
 
-        if DO_NOT_PRINT:
-            print(file.stream.read())
-            return "true"
-
-        print_process = Popen('lp', stdin=PIPE, stdout=PIPE, stderr=PIPE, shell=True)
-
-        # Send file to print process via stdin
-        while block := file.stream.read(1024):
-            print_process.stdin.write(block)
-
-        stdout, stderr = print_process.communicate()
-
-        # Check of print process is happy
-        exitcode = print_process.wait()
-
-        if exitcode != 0:
-            print(f'{exitcode}: {stderr.decode()}')
-            return "false"
-
+    # Create print process
+    if DO_NOT_PRINT:
+        print(file.stream.read())
         return "true"
+
+    print_process = Popen('lp', stdin=PIPE, stdout=PIPE, stderr=PIPE, shell=True)
+
+    # Send file to print process via stdin
+    while block := file.stream.read(1024):
+        print_process.stdin.write(block)
+
+    stdout, stderr = print_process.communicate()
+
+    # Check of print process is happy
+    exitcode = print_process.wait()
+
+    if exitcode != 0:
+        print(f'{exitcode}: {stderr.decode()}')
+        return "false"
+
+    return "true"
 
 
 if __name__ == '__main__':
